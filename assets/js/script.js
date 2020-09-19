c1b3ae53ee9618f1218af920b0f7d86865591196
Ray Bray <rbray.bray2@gmail.com>
2020-09-19 00:00:43 -0400

    Appended both cards to the portfolio page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 console.log("Hello World");
 
+//Display Weather Dashboard App to the page
 var card = $("<div>").addClass("card text center");
 var cardHeader = $("<div>")
   .addClass("card-header text-center")
@@ -12,9 +13,29 @@ var cardImage = $("<img>")
     "file:///Users/rayshawnbray/Desktop/Screen%20Shot%202020-09-17%20at%2010.31.45%20PM.png"
   )
   .attr("style", "width: 500px")
-//   .addClass("ml-4");
 
 //Append to Document
 cardBody.append(cardHeader, cardImage);
 card.append(cardBody);
 $("#image").append(card);
+
+//================================================
+//Display Workday scheduler to the Page
+var card2 = $("<div>").addClass("card text center mt-2");
+var cardHeader2 = $("<div>")
+  .addClass("card-header text-center")
+  .text("Workday Scheduler")
+
+var cardBody2 = $("<div>").addClass("card-body")
+
+var cardImage2 = $("<img>")
+  .attr(
+    "src",
+    "file:///Users/rayshawnbray/Desktop/Screen%20Shot%202020-09-12%20at%206.50.24%20PM.png"
+  )
+  .attr("style", "width: 500px")
+
+//Append to Document
+cardBody2.append(cardHeader2, cardImage2);
+card2.append(cardBody2);
+$("#image").append(card2);
