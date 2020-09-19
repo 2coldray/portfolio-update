27644e89075c7190757d97cb5a1f37ade3308931
Ray Bray <rbray.bray2@gmail.com>
2020-09-18 23:52:01 -0400

    appended card to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 console.log("Hello World");
 
-var weather = $("<img>")
+var card = $("<div>").addClass("card text center");
+var cardHeader = $("<div>")
+  .addClass("card-header text-center")
+  .text("Weather Dashboard Application");
+var cardBody = $("<div>").addClass("card-body")
+
+var cardImage = $("<img>")
   .attr(
     "src",
     "file:///Users/rayshawnbray/Desktop/Screen%20Shot%202020-09-17%20at%2010.31.45%20PM.png"
   )
   .attr("style", "width: 500px")
-  .addClass("ml-4");
+//   .addClass("ml-4");
 
 //Append to Document
-$("#image").append(weather);
+cardBody.append(cardHeader, cardImage);
+card.append(cardBody);
+$("#image").append(card);
