9ba50a5eaf840f5cc02e9df9c7ac30d37c201c8b
Ray Bray <rbray.bray2@gmail.com>
2020-09-18 23:38:58 -0400

    added first image to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+console.log("Hello World");
+
+var weather = $("<img>")
+  .attr(
+    "src",
+    "file:///Users/rayshawnbray/Desktop/Screen%20Shot%202020-09-17%20at%2010.31.45%20PM.png"
+  )
+  .attr("style", "width: 500px")
+  .addClass("ml-4");
+
+//Append to Document
+$("#image").append(weather);
